93e7b3d050b3ecedbfd7aba91383882edc3a4318
vak-01 <78302047+vak-01@users.noreply.github.com>
2023-10-31 23:44:40 +0530

    minor change #1

--- src/main/java/com/maven/LAB_MID_TERM/CredentialChecker.java
+++ src/main/java/com/maven/LAB_MID_TERM/CredentialChecker.java
@@ -2,6 +2,7 @@ package com.maven.LAB_MID_TERM;
 
 import java.util.ResourceBundle;
 
+// this is a simple change - one comment was added
 public class CredentialChecker {
 	private ResourceBundle resourceBundle;
 
